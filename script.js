0717b72214c331b568cfa724193e290bd5febc3d
StevenT99 <96297488+StevenT99@users.noreply.github.com>
2023-10-30 12:24:42 -0600

    js update for style

--- script.js
+++ script.js
@@ -1,47 +1,65 @@
 // Get references to the HTML elements
 const contentParagraph = document.getElementById("content");
-const changeButton = document.getElementById("changeButton");
 
 // Define a function to change the content
 function changeContent() {
   contentParagraph.textContent = "The content has been changed!";
 }
 
-// Add a click event listener to the button
-changeButton.addEventListener("click", changeContent);
-
-//add amount to piggy bank
-// Initialize the current savings amount
-var currentSavings = 0; // Initial savings amount
-
+// Update Goal
 function updateGoal() {
   const goalAmount = parseFloat(document.getElementById("goal-input").value);
-  document.getElementById("goal-amount").textContent = goalAmount;
+  document.getElementById("goal-amount").textContent = goalAmount.toFixed(2); // Display with two decimal places
 }
 
+// Add Money
 function addMoney() {
-  // Update the current savings
-  document.getElementById("savings-amount").textContent = currentSavings;
+  var currentSavings = parseFloat(
+    document.getElementById("savings-amount").textContent
+  );
+  var amountToAdd = parseFloat(document.getElementById("add-money").value);
 
-  var amounttoadd = parseFloat(document.getElementById("add-money").value);
-  currentSavings += amounttoadd; // Add the amount to current savings
-  document.getElementById("savings-amount").textContent = currentSavings;
-  const totalGoal = parseFloat(document.getElementById("goal-input").value); // Total goal amount
-  const percentage = (currentSavings / totalGoal) * 100;
+  if (!isNaN(amountToAdd)) {
+    currentSavings += amountToAdd; // Add the amount to current savings
+    document.getElementById("savings-amount").textContent =
+      currentSavings.toFixed(2); // Display with two decimal places
+    const totalGoal = parseFloat(
+      document.getElementById("goal-amount").textContent
+    );
+    const percentage = (currentSavings / totalGoal) * 100;
 
-  if (percentage >= 100) {
-    alert("You have met your goal!");
+    if (percentage >= 100) {
+      alert("Congratulations! You have met your goal!");
+    }
   }
 }
 
+// Withdraw Money
 function withdrawMoney() {
-  var amounttowithdraw = parseFloat(
+  var currentSavings = parseFloat(
+    document.getElementById("savings-amount").textContent
+  );
+  var amountToWithdraw = parseFloat(
     document.getElementById("withdraw-money").value
   );
-  if (amounttowithdraw <= currentSavings) {
-    currentSavings -= amounttowithdraw; // Withdraw the amount from current savings
-    document.getElementById("savings-amount").textContent = currentSavings;
-  } else {
-    alert("You cannot withdraw more than your current savings.");
+
+  if (!isNaN(amountToWithdraw)) {
+    if (amountToWithdraw <= currentSavings) {
+      currentSavings -= amountToWithdraw; // Withdraw the amount from current savings
+      document.getElementById("savings-amount").textContent =
+        currentSavings.toFixed(2); // Display with two decimal places
+    } else {
+      alert("You cannot withdraw more than your current savings.");
+    }
   }
 }
+
+// Add event listeners
+document
+  .getElementById("changeButton")
+  .addEventListener("click", changeContent);
+document.getElementById("goal-input").addEventListener("change", updateGoal);
+document.getElementById("add-money").addEventListener("change", addMoney);
+document
+  .getElementById("withdraw-money")
+  .addEventListener("change", withdrawMoney);
